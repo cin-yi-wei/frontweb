0e5b19ca2db9e8858a598bbc1716f16a991ec28d
cin-yi-wei <m95162027@gmail.com>
2022-03-07 13:45:49 +0800

    add javascript

---
+++ javascript/app.js
@@ -0,0 +1,11 @@
+window.onload = () => {
+  console.log("qqq");
+
+  var h1 = document.createElement("h1");
+  h1.innerHTML = "This is my h1!";
+  document.body.appendChild(h1);
+  var _x = document.createElement("h1");
+
+  const age = ["a"];
+  for (var i = 0; i < 100000000000; i++) age.push(1);
+};
