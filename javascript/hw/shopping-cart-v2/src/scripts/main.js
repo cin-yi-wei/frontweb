d908471b45bb818bff4f8ed8c82e617845a2396c
cin-yi-wei <m95162027@gmail.com>
2022-03-29 00:58:04 +0800

    init

--- javascript/hw/shopping-cart-v2/src/scripts/main.js
+++ javascript/hw/shopping-cart-v2/src/scripts/main.js
@@ -1,24 +1,105 @@
 import 'bootstrap/dist/css/bootstrap.css'
 import '@fortawesome/fontawesome-free/css/all.css'
-// 實作寫在這裡！
+
+const addListener = (upComponent,downComponent,listener,fn) => {
+	listener.addEventListener("click",(e)=>{
+		let target;
+		let nodeName = e.target.nodeName;
+		if( nodeName == downComponent) target = e.target.parentNode;
+		if( nodeName == upComponent) target = e.target;
+		if( nodeName == upComponent || nodeName == downComponent){
+			fn(target);
+		}
+	})
+}
+
+addListener("BUTTON","I",document.querySelector(".container > .items"),(target) => {
+	let name = target.parentNode.children[0].textContent;
+	let price = target.parentNode.children[1].textContent;
+	let cartList = document.querySelectorAll("tbody tr td:first-child");
+	let existInCartIndex = Array.from(cartList).findIndex(node => node.textContent == name);
+
+	if(existInCartIndex > -1){
+		let num = cartList[existInCartIndex].parentNode.children[1].children[0];
+		var itemPrice = cartList[existInCartIndex].parentNode.children[3];
+		num.value++;
+		itemPrice.textContent = "$" +(Number(itemPrice.textContent.slice(1)) 
+									+ Number(price.slice(1))).toString();
+	}else{
+		let insert = `
+					<tr>
+					<td>${name}</td>
+					<td><input type="number" value="1" /></td>
+					<td>${price}</td>
+					<td>${price}</td>
+					<td>
+						<button class="btn btn-danger btn-sm">
+						<i class="fas fa-trash-alt"></i>
+						</button>
+					</td>
+					</tr>
+					`
+		document.querySelector("tbody").insertAdjacentHTML("beforeend",insert);
+	}
+	let allPrice =  document.querySelector("tfoot td:nth-child(3)");
+	allPrice.textContent = "$" + (Number(allPrice.textContent.slice(1))
+									+Number(price.slice(1))).toString();
+
+})
+
 
 /*
-console.log( document.querySelector(".container > .items") );
 
 document.querySelector(".container > .items").addEventListener("click",(e)=>{
-    console.log(e.target,e.target.parentNode.children[0].textcontent ,e.target.parentNode.children[1]);
-    
+	let target;
+	let nodeName = e.target.nodeName;
+	if( nodeName == "I") target = e.target.parentNode;
+	if( nodeName == "BUTTON") target = e.target;
+	if( nodeName == "BUTTON" || nodeName == "I"){
+		let name = target.parentNode.children[0].textContent;
+		let price = target.parentNode.children[1].textContent;
+		let cartList = document.querySelectorAll("tbody tr td:first-child");
+		let existInCartIndex = Array.from(cartList).findIndex(node => node.textContent == name);
+
+		if(existInCartIndex > -1){
+			let num = cartList[existInCartIndex].parentNode.children[1].children[0];
+			var itemPrice = cartList[existInCartIndex].parentNode.children[3];
+			num.value++;
+			itemPrice.textContent = "$" +(Number(itemPrice.textContent.slice(1)) 
+										+ Number(price.slice(1))).toString();
+		}else{
+			let insert = `
+						<tr>
+						<td>${name}</td>
+						<td><input type="number" value="1" /></td>
+						<td>${price}</td>
+						<td>${price}</td>
+						<td>
+							<button class="btn btn-danger btn-sm">
+							<i class="fas fa-trash-alt"></i>
+							</button>
+						</td>
+						</tr>
+						`
+			document.querySelector("tbody").insertAdjacentHTML("beforeend",insert);
+		}
+		let allPrice =  document.querySelector("tfoot td:nth-child(3)");
+		allPrice.textContent = "$" + (Number(allPrice.textContent.slice(1))
+									 +Number(price.slice(1))).toString();
+	}
 })
 */
-document.querySelector("tbody").addEventListener("click",(e)=>{
-   if(e.target.nodeName == "I") e.target.parentNode.parentNode.parentNode.remove();
-   else e.target.parentNode.parentNode.remove();
-   console.log(e.target.nodeName);
-})
+
+
+
+
+// document.querySelector("tbody").addEventListener("click",(e)=>{
+//    if(e.target.nodeName == "I") e.target.parentNode.parentNode.parentNode.remove();
+//    else e.target.parentNode.parentNode.remove();
+//    console.log(e.target.nodeName);
+// })
 
 /*
- <section class="cart">
-<table class="table">
 <tbody>
 <tr>
   <td>老大</td>
